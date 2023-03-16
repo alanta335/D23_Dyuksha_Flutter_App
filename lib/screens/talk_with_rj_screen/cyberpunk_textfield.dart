bfc5bada40f43e51aa47b8fca0b2288afad30d8b
Harikrishnan B A <harikrishnanasok@gmail.com>
2023-03-16 18:25:28 +0530

    Created Talk with RJ screen with text editing fields

---
+++ lib/screens/talk_with_rj_screen/cyberpunk_textfield.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CyberpunkTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final int maxLines;
+  final String label;
+
+  const CyberpunkTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    this.maxLines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: GoogleFonts.chakraPetch(
+            color: Colors.yellow,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 12.0),
+        TextField(
+          minLines: maxLines,
+          maxLines: maxLines != 1 ? maxLines + 4 : 1,
+          style: GoogleFonts.chakraPetch(
+            color: Colors.yellow,
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+          ),
+          cursorColor: Colors.yellow,
+          controller: controller,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.yellow.withOpacity(0.3),
+            enabledBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.zero,
+              borderSide: BorderSide(
+                color: Colors.yellow,
+                width: 1,
+              ),
+            ),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.zero,
+              borderSide: BorderSide(
+                color: Colors.yellow,
+                width: 1,
+              ),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderRadius: BorderRadius.zero,
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 1,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
