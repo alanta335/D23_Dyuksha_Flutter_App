6923e296347ee8c4e28b290020abb92fa7c04498
alanta335 <alanta335@gmail.com>
2023-03-16 12:21:09 +0530

    login added

---
+++ lib/mainscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class mainScreen extends StatefulWidget {
+  const mainScreen({super.key});
+
+  @override
+  State<mainScreen> createState() => _mainScreenState();
+}
+
+class _mainScreenState extends State<mainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
