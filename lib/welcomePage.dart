21f60fefa5e33de47cbcd0e011b62588afb8435e
Akhil Krishna S <s.akhilkrishna@gmail.com>
2023-03-20 21:34:29 +0530

    Second release

--- lib/welcomePage.dart
+++ lib/welcomePage.dart
@@ -23,7 +23,7 @@ class _WelcomePageState extends State<WelcomePage> {
   final _pageViewController = PageController();
 
   int _currentIndex = 0;
-
+  int counter = 0;
   @override
   void initState() {
     super.initState();
@@ -37,7 +37,7 @@ class _WelcomePageState extends State<WelcomePage> {
     });
     if (mounted) {
       confettiControllerLeft.addListener(() {
-        Future.delayed(const Duration(seconds: 2), () {
+        Future.delayed(const Duration(seconds: 4), () {
           _showDialog(context);
         });
       });
@@ -61,128 +61,140 @@ class _WelcomePageState extends State<WelcomePage> {
   }
 
   void _showDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (_) => Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-            image: AssetImage("assets/images/cyber_city.jpg"),
-            fit: BoxFit.cover,
-          )),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 5,
-              sigmaY: 5,
-            ),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      const Spacer(),
-                      GlitchEffect(
-                        duration: const Duration(seconds: 3),
-                        colors: const [
-                          Colors.black,
-                          Colors.red,
-                          Colors.blue,
-                          Colors.purple
-                        ],
-                        child: Text(
-                          "CREDITS",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.chakraPetch(
-                            color: Colors.white,
-                            fontSize: 55,
-                            fontWeight: FontWeight.w700,
-                            decoration: TextDecoration.underline,
+    print(context);
+    if (counter == 1) {
+      setState(() {
+        counter = 0;
+      });
+    } else {
+      setState(() {
+        counter = 1;
+      });
+      showDialog(
+        context: context,
+        builder: (_) => Scaffold(
+          body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage("assets/images/cyber_city.jpg"),
+              fit: BoxFit.cover,
+            )),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 5,
+                sigmaY: 5,
+              ),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        const Spacer(),
+                        GlitchEffect(
+                          duration: const Duration(seconds: 3),
+                          colors: const [
+                            Colors.black,
+                            Colors.red,
+                            Colors.blue,
+                            Colors.purple
+                          ],
+                          child: Text(
+                            "CREDITS",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.chakraPetch(
+                              color: Colors.white,
+                              fontSize: 55,
+                              fontWeight: FontWeight.w700,
+                              decoration: TextDecoration.underline,
+                            ),
                           ),
                         ),
-                      ),
-                      const Spacer(),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    height: MediaQuery.of(context).size.height * 0.07,
-                    child: AnimatedText(
-                      alignment: Alignment.center,
-                      speed: const Duration(milliseconds: 1000),
-                      controller: AnimatedTextController.loop,
-                      displayTime: const Duration(milliseconds: 1000),
-                      wordList: const [
-                        'DEVELOPERS',
-                        'HARIKRISHNAN B A',
-                        'ALAN T A',
-                        'AKHIL KRISHNA',
-                        'ANOOP P K',
-                        'DESIGN TEAM',
-                        'ABHIN SURESH S',
-                        'ALEENA KT',
-                        'SPECIAL MENTION',
-                        'ABHISHEK P V',
-                        'JOSEPH FERNANDEZ',
+                        const Spacer(),
                       ],
-                      textStyle: GoogleFonts.merienda(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontWeight: FontWeight.w700,
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      child: AnimatedText(
+                        alignment: Alignment.center,
+                        speed: const Duration(milliseconds: 1000),
+                        controller: AnimatedTextController.loop,
+                        displayTime: const Duration(milliseconds: 1000),
+                        wordList: const [
+                          'DEVELOPERS',
+                          'HARIKRISHNAN B A',
+                          'ALAN T A',
+                          'AKHIL KRISHNA S',
+                          'ANOOP P K',
+                          'DESIGN TEAM',
+                          'ABHIN SURESH S',
+                          'ALEENA KT',
+                          'SHYAM KRISHNAN',
+                          'SPECIAL MENTION',
+                          'ABHISHEK P V',
+                          'JOSEPH FERNANDEZ',
+                        ],
+                        textStyle: GoogleFonts.merienda(
+                          color: Colors.white,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 5),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    height: MediaQuery.of(context).size.height * 0.07,
-                    child: AnimatedText(
-                      alignment: Alignment.center,
-                      speed: const Duration(milliseconds: 1000),
-                      controller: AnimatedTextController.loop,
-                      displayTime: const Duration(milliseconds: 1000),
-                      wordList: const [
-                        '',
-                        '@me.harikrish',
-                        '@alan_ta_335',
-                        '@ilmentore72',
-                        '@ichbin_anoop',
-                        '',
-                        '@kiyoshi_abhin',
-                        '@aleena_k_t',
-                        '',
-                        '@_abishek_pv',
-                        '@joseph._fernandez',
-                      ],
-                      textStyle: GoogleFonts.cutiveMono(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
+                    const SizedBox(height: 5),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      child: AnimatedText(
+                        alignment: Alignment.center,
+                        speed: const Duration(milliseconds: 1000),
+                        controller: AnimatedTextController.loop,
+                        displayTime: const Duration(milliseconds: 1000),
+                        wordList: const [
+                          '',
+                          '@me.harikrish',
+                          '@alan_ta_335',
+                          '@ilmentore72',
+                          '@ichbin_anoop',
+                          '',
+                          '@kiyoshi_abhin',
+                          '@aleena_k_t',
+                          '@shhyaaam',
+                          '',
+                          '@_abishek_pv',
+                          '@joseph._fernandez',
+                        ],
+                        textStyle: GoogleFonts.cutiveMono(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 60),
-                  IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: const Icon(
-                        Icons.exit_to_app,
-                        color: Colors.white,
-                        size: 42,
-                      ))
-                ],
+                    const SizedBox(height: 60),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(
+                          Icons.exit_to_app,
+                          color: Colors.white,
+                          size: 42,
+                        ))
+                  ],
+                ),
               ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 
   @override
