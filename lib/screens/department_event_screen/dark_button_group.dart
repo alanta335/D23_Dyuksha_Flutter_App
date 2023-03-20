3af457e32363fb02f0846e20b70b03c198520e5d
Harikrishnan B A <harikrishnanasok@gmail.com>
2023-03-20 13:19:58 +0530

    Added pdf downloading feature and added registration url for share

--- lib/screens/department_event_screen/dark_button_group.dart
+++ lib/screens/department_event_screen/dark_button_group.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:printing/printing.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 import '../../models/event.dart';
 import 'dark_button.dart';
 
@@ -30,12 +35,149 @@ class DarkButtonGroup extends StatelessWidget {
 
   Future<void> _share() async {
     await Share.share(
-      "*****DYUKSHA'23*****\n\n${event.name}\n\nABOUT\n\n${event.about}\n\nCoordinator:${event.coordintorName}\nContact:${event.contact}",
+      "*****DYUKSHA'23*****\n\n${event.name}\n\nABOUT\n\n${event.about}\n\nCoordinator:${event.coordintorName}\nContact:${event.contact}\nRegistration URL:${event.registrationURL}\n",
       subject: event.name,
     );
   }
 
-  Future<void> _download() async {}
+  Future<void> _download() async {
+    try {
+      final output = await getDownloadsDirectory();
+      if (output == null) {
+        return;
+      }
+
+      final pdf = pw.Document();
+      final fontStyleBold = await PdfGoogleFonts.chakraPetchBold();
+      final fontStyleRegular = await PdfGoogleFonts.chakraPetchRegular();
+      final fontStyleSemiBold = await PdfGoogleFonts.chakraPetchSemiBold();
+
+      final eventImage = await networkImage(event.imageURL);
+      File file = File("${output.path}/dyuksha23_${event.name}.pdf");
+
+      pdf.addPage(
+        pw.MultiPage(
+          pageFormat: PdfPageFormat.a4,
+          margin: const pw.EdgeInsets.all(20.0),
+          footer: (_) => pw.Padding(
+            padding: const pw.EdgeInsets.symmetric(vertical: 12.0),
+            child: pw.Text(
+              "Dyuksha '23",
+              style: pw.TextStyle(
+                font: fontStyleBold,
+                color: PdfColor.fromInt(Colors.black.value),
+              ),
+            ),
+          ),
+          build: (pw.Context context) {
+            return [
+              pw.Partitions(
+                children: [
+                  pw.Partition(
+                    child: pw.Container(
+                      padding: const pw.EdgeInsets.all(12.0),
+                      decoration: pw.BoxDecoration(
+                        border: pw.Border.all(
+                          color: PdfColor.fromInt(event.getColorOfDay().value),
+                        ),
+                      ),
+                      child: pw.Column(
+                        crossAxisAlignment: pw.CrossAxisAlignment.stretch,
+                        verticalDirection: pw.VerticalDirection.down,
+                        mainAxisSize: pw.MainAxisSize.max,
+                        children: <pw.Widget>[
+                          pw.Text(
+                            event.name.toUpperCase(),
+                            textAlign: pw.TextAlign.left,
+                            style: pw.TextStyle(
+                              font: fontStyleBold,
+                              fontSize: 42.0,
+                              color:
+                                  PdfColor.fromInt(event.getColorOfDay().value),
+                            ),
+                          ),
+                          pw.SizedBox(height: 16.0),
+                          pw.ClipRRect(
+                            child: pw.Image(
+                              eventImage,
+                            ),
+                            horizontalRadius: 20.0,
+                            verticalRadius: 20.0,
+                          ),
+                          pw.SizedBox(height: 16.0),
+                          pw.Text(
+                            "DAY ${event.day}\t\t${event.time}",
+                            style: pw.TextStyle(
+                              font: fontStyleRegular,
+                              fontSize: 24.0,
+                              color: PdfColor.fromInt(Colors.grey.value),
+                            ),
+                          ),
+                          pw.SizedBox(height: 80.0),
+                          pw.Text(
+                            "ABOUT",
+                            style: pw.TextStyle(
+                              font: fontStyleSemiBold,
+                              fontSize: 18.0,
+                              decoration: pw.TextDecoration.underline,
+                              color: PdfColor.fromInt(Colors.black.value),
+                            ),
+                          ),
+                          pw.SizedBox(height: 8.0),
+                          pw.Text(
+                            "\n\t${event.about}",
+                            textAlign: pw.TextAlign.justify,
+                            style: pw.TextStyle(
+                              font: fontStyleSemiBold,
+                              fontSize: 14.0,
+                              color: PdfColor.fromInt(Colors.black.value),
+                            ),
+                          ),
+                          pw.SizedBox(height: 12.0),
+                          pw.Text(
+                            "COORDINATOR : ${event.coordintorName}",
+                            style: pw.TextStyle(
+                              font: fontStyleSemiBold,
+                              fontSize: 16.0,
+                              color: PdfColor.fromInt(Colors.white.value),
+                            ),
+                          ),
+                          pw.Text(
+                            "CONTACT : ${event.contact}",
+                            style: pw.TextStyle(
+                              font: fontStyleSemiBold,
+                              fontSize: 16.0,
+                              color: PdfColor.fromInt(Colors.white.value),
+                            ),
+                          ),
+                          pw.SizedBox(height: 6.0),
+                          pw.UrlLink(
+                            child: pw.Text(
+                              "REGISTER NOW",
+                              style: pw.TextStyle(
+                                font: fontStyleBold,
+                                decoration: pw.TextDecoration.underline,
+                                fontSize: 26.0,
+                                color: PdfColor.fromInt(
+                                    event.getColorOfDay().value),
+                              ),
+                            ),
+                            destination: event.registrationURL,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ];
+          },
+        ),
+      );
+
+      await file.writeAsBytes(await pdf.save());
+    } catch (e) {}
+  }
 
   @override
   Widget build(BuildContext context) {
